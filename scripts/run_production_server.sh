b04b71258cc32242c0c9638841e5c2ed8d007e3f
sjbriley <sbriley.0@gmail.com>
2024-10-30 21:27:32 -0600

    Fix automation scripts

--- scripts/run_production_server.sh
+++ scripts/run_production_server.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -x
+
+# check if grpc host pasesd in
+if [ -z "$1" ]; then
+  echo "Usage: $0 <GRP_HOST>"
+  exit 1
+fi
+
+GRP_HOST=$1
 EXPORT PROD=1
 source dl-env/bin/activate
-python
+python dl-env/bin/python train_server.py -a alexnet --gpu 0 --batch-size 8 --epochs 50 --grpc-host "$GRP_HOST"
 echo "Production servers are running."
--- scripts/run_production_storage.sh
+++ scripts/run_production_storage.sh
@@ -2,5 +2,5 @@
 set -x
 EXPORT PROD=1
 source dl-env/bin/activate
-python
+python dl-env/bin/python storage_server.py --offloading 0 --compression 0 --batch_size 8
 echo "Production servers are running."
